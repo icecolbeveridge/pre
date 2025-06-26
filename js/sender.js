ac49bc117297369b49e49ad587211dc1dbccb2d1
Colin Beveridge <colin@flyingcoloursmaths.co.uk>
2025-06-26 16:42:53 +0100

    add questionnaire

--- js/sender.js
+++ js/sender.js
@@ -1,14 +1,24 @@
-<script>
+// Wait for DOM to be fully loaded
+document.addEventListener("DOMContentLoaded", function () {
   (function (s, e, n, d, er) {
-    s['Sender'] = er;
-    s[er] = s[er] || function () {
-      (s[er].q = s[er].q || []).push(arguments)
-    }, s[er].l = 1 * new Date();
+    s["Sender"] = er;
+    ((s[er] =
+      s[er] ||
+      function () {
+        (s[er].q = s[er].q || []).push(arguments);
+      }),
+      (s[er].l = 1 * new Date()));
     var a = e.createElement(n),
-        m = e.getElementsByTagName(n)[0];
+      m = e.getElementsByTagName(n)[0];
     a.async = 1;
     a.src = d;
-    m.parentNode.insertBefore(a, m)
-  })(window, document, 'script', 'https://cdn.sender.net/accounts_resources/universal.js', 'sender');
-  sender('d975ddf2228337')
-</script>
+    m.parentNode.insertBefore(a, m);
+  })(
+    window,
+    document,
+    "script",
+    "https://cdn.sender.net/accounts_resources/universal.js",
+    "sender",
+  );
+  sender("d975ddf2228337");
+});
